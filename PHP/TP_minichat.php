6565a10bebc2d8f459210add78d41a9d6cb97695
Sevhalen <sevhalen@gmail.com>
2017-09-01 19:22:36 +0200

    fin du TP minichat from SPF

--- PHP/TP_minichat.php
+++ PHP/TP_minichat.php
@@ -14,16 +14,41 @@
     </style>
     
     <body>
-        
-    
-<?php
+        <form action="TP_minichat_post.php" method="post">
+            <p>                
+                <label for="pseudo">Pseudo : </label><input type='text' name="pseudo" id="pseudo">
+                <label for="message">Message : </label><input type='text' name="message" id="message">
+                <input type="submit" value="Envoyer">
+            </p>
+        </form>
 
-if(isset($_POST['Pseudo']) && isset($_POST['Message']))
-{
-    
-}
-
-?>
+        <h3>Minichat</h3>
         
+        <?php
+     
+            // Recuperation des messages de la BdD
+            try
+            {
+                // Connexion a la bdd
+                $bdd = new PDO('mysql:host=localhost;dbname=OCR_test;charset=utf8', 'root', 'root');
+            } catch (Exception $ex)
+            {
+                // En cas d'erreur, affichage d'un message et arret de l'application
+                die('Erreur : ' . $ex->getMessage());
+            }
+            
+            $reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC Limit 0, 10');
+            
+            if($reponse) {            
+            while($donnees = $reponse->fetch())
+            {
+                ?>
+                <p>
+                <strong><?= $donnees['pseudo'] . ' - '; ?></strong>
+                <?= $donnees['message']; ?>
+                </p>
+            <?php
+            }}
+            ?>
     </body>
 </html>
---
+++ PHP/TP_minichat_post.php
@@ -0,0 +1,29 @@
+<?php
+
+$db_login = 'root';
+$db_pw    = 'root';
+$new_m_pseudo = htmlspecialchars($_POST['pseudo']);
+$new_message = htmlspecialchars($_POST['message']);
+
+var_dump($_POST['pseudo']);
+var_dump($_POST['message']);
+    
+try
+{
+    // Connexion a la bdd
+    $bdd = new PDO('mysql:host=localhost;dbname=OCR_test;charset=utf8', $db_login, $db_pw);
+} catch (Exception $ex)
+{
+    // En cas d'erreur, affichage d'un message et arret de l'application
+    die('Erreur : ' . $ex->getMessage());
+}
+
+// Ajout d'un message supplementaire dans la BdD
+if(isset($_POST['pseudo']) && isset($_POST['message']))
+{
+    $req = $bdd->prepare('INSERT INTO minichat(pseudo, message) VALUES(?, ?)');
+    $req->execute(array($new_m_pseudo, $new_message));
+}
+
+header('location: TP_minichat.php');
+?>
